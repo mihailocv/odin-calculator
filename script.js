3199e1ce70c119e3c5795118c602bd2b84552698
mihailocv <127780035+mihailocv@users.noreply.github.com>
2024-06-15 18:51:19 +0200

    New script, displaying numbers working

--- script.js
+++ script.js
@@ -1,90 +1,126 @@
-const resultArea = document.querySelector("#result");
-const numberArea = document.querySelector("#numbers");
-const operatorArea = document.querySelector("#operators");
-const specialArea = document.querySelector("#specials");
+const calculator = document.querySelector(".calculator");
+const display = document.querySelector(".calculator__display");
+const buttons = document.querySelector(".calculator__buttons");
 
-let firsNum = null;
-let secondNum = null;
-let operator = null;
-let displayValue = "";
+// let firsNum = null;
+// let secondNum = null;
+// let operator = null;
+// let displayValue = "";
+let numOnDisplay = display.textContent;
 
-function display(button) {
-  let target = button.target.innerText;
-  if (displayValue.length !== 9) {
-    displayValue += target;
+function displayNumbers(e) {
+  const targetContent = e.target.textContent;
+  if (numOnDisplay.length < 9) {
+    if (numOnDisplay === "0") {
+      numOnDisplay = targetContent;
+    } else {
+      numOnDisplay += targetContent;
+    }
+    display.textContent = numOnDisplay;
   }
-  resultArea.textContent = displayValue;
 }
 
-const add = function (num1, num2) {
-  return num1 + num2;
-};
+// const add = function (num1, num2) {
+//   return num1 + num2;
+// };
+//
+// const subtract = function (num1, num2) {
+//   return num1 - num2;
+// };
+//
+// const multiply = function (num1, num2) {
+//   return num1 * num2;
+// };
+//
+// const divide = function (num1, num2) {
+//   return num2 !== 0 ? num1 / num2 : "ERROR!";
+// };
+//
+// const operate = function (num1, operator, num2) {
+//   return operator(num1, num2);
+// };
 
-const subtract = function (num1, num2) {
-  return num1 - num2;
-};
-
-const multiply = function (num1, num2) {
-  return num1 * num2;
-};
-
-const divide = function (num1, num2) {
-  return num2 !== 0 ? num1 / num2 : "ERROR!";
-};
-
-const operate = function (num1, operator, num2) {
-  return operator(num1, num2);
-};
-
-numberArea.addEventListener("click", (e) => {
+buttons.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
-    display(e);
-  }
-});
+    const target = e.target;
+    const action = target.dataset.action;
+    const targetContent = target.textContent;
+    const numOnDisplay = display.textContent;
+    let operator = null;
 
-operatorArea.addEventListener("click", (e) => {
-  let target = e.target;
-  switch (target.innerText) {
-    case "+":
-      operator = add;
-      firsNum = parseInt(displayValue);
-      resultArea.textContent = firsNum;
-      displayValue = "";
-      break;
-    case "-":
-      operator = subtract;
-      firsNum = parseInt(displayValue);
-      resultArea.textContent = firsNum;
-      displayValue = "";
-      break;
-    case "*":
-      operator = multiply;
-      firsNum = parseInt(displayValue);
-      resultArea.textContent = firsNum;
-      displayValue = "";
-      break;
-    case "/":
-      operator = divide;
-      firsNum = parseInt(displayValue);
-      resultArea.textContent = firsNum;
-      displayValue = "";
-      break;
-    case "=":
-      if (operator !== null) {
-        secondNum = parseInt(displayValue);
-        resultArea.textContent = operate(firsNum, operator, secondNum);
+    if (!action) {
+      displayNumbers(e);
+    }
+    if (
+      action === "divide" ||
+      action === "multiply" ||
+      action === "subtract" ||
+      action === "add"
+    ) {
+      target.classList.add("active");
+      operator = action;
+      console.log(operator);
+    }
+    if (action === "clear") {
+      console.log("I'm clear button!");
+    }
+    if (action === "decimal") {
+      if (!numOnDisplay.includes(",")) {
+        display.textContent = numOnDisplay + targetContent;
       }
-      break;
+    }
+    if (action === "equal") {
+      console.log("I'm equal button!");
+    }
+    Array.from(target.parentNode.children).forEach((t) =>
+      t.classList.remove("active"),
+    );
   }
 });
 
-specialArea.addEventListener("click", (e) => {
-  let target = e.target.innerText;
-  if (target === "C") {
-    resultArea.textContent = "0";
-    displayValue = "";
-    operator = null;
-    firsNum = null;
-    secondNum = null;
-  }
-});
+// operatorArea.addEventListener("click", (e) => {
+//   let target = e.target;
+//   switch (target.innerText) {
+//     case "+":
+//       operator = add;
+//       firsNum = parseInt(displayValue);
+//       resultArea.textContent = firsNum;
+//       displayValue = "";
+//       break;
+//     case "-":
+//       operator = subtract;
+//       firsNum = parseInt(displayValue);
+//       resultArea.textContent = firsNum;
+//       displayValue = "";
+//       break;
+//     case "*":
+//       operator = multiply;
+//       firsNum = parseInt(displayValue);
+//       resultArea.textContent = firsNum;
+//       displayValue = "";
+//       break;
+//     case "/":
+//       operator = divide;
+//       firsNum = parseInt(displayValue);
+//       resultArea.textContent = firsNum;
+//       displayValue = "";
+//       break;
+//     case "=":
+//       if (operator !== null) {
+//         secondNum = parseInt(displayValue);
+//         resultArea.textContent = operate(firsNum, operator, secondNum);
+//       }
+//       break;
+//   }
+// });
+//
+// specialArea.addEventListener("click", (e) => {
+//   let target = e.target.innerText;
+//   if (target === "C") {
+//     resultArea.textContent = "0";
+//     displayValue = "";
+//     operator = null;
+//     firsNum = null;
+//     secondNum = null;
+//   }
+// });
